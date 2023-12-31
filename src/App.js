09da2e8a8a446269cc46e5dc45eb211c5e94c892
MuhammadUmair123456789 <132666833+MuhammadUmair123456789@users.noreply.github.com>
2023-12-31 18:33:37 +0500

    1st

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Headere from '../src/component/Headere';
+
+function App() {
+  return (
+    <>
+      <Headere />
+
+    </>
+      );
+}
+
+export default App;
